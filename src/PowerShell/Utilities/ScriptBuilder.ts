f39f512a42afd898fe3bb0afc6888bcdc2773722
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-05-18 16:47:06 +0530

    added time taken for script

--- src/PowerShell/Utilities/ScriptBuilder.ts
+++ src/PowerShell/Utilities/ScriptBuilder.ts
@@ -16,20 +16,19 @@ export default class ScriptBuilder {
                 command += `Set-AzContext -SubscriptionId ${args.subscriptionId} -TenantId ${tenantId} | out-null;`;
             }
         }
-        this.script += `
-        $curr = [system.diagnostics.stopwatch]::startNew()
-        try {
+        this.script += `try {
+            $curr = [system.diagnostics.stopwatch]::startNew()
             $ErrorActionPreference = "Stop"
             $WarningPreference = "SilentlyContinue"
             $output = @{}
             ${command}
             $output['${Constants.Success}'] = "true"
+            $secs = $curr.Elapsed.TotalSeconds
+            echo "time elapsed: $secs"
         }
         catch {
             $output['${Constants.Error}'] = $_.exception.Message
         }
-        $secs = $curr.Elapsed.TotalSeconds
-        echo "time elapsed: $secs"
         return ConvertTo-Json $output`;
         core.debug(`Azure PowerShell Login Script: ${this.script}`);
         return this.script;
@@ -37,21 +36,20 @@ export default class ScriptBuilder {
 
     getLatestModuleScript(moduleName: string): string {
         const command: string = `Get-Module -Name ${moduleName} -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1`;
-        this.script += `
-        $curr = [system.diagnostics.stopwatch]::startNew()
-        try {
+        this.script += `try {
+            $curr = [system.diagnostics.stopwatch]::startNew()
             $ErrorActionPreference = "Stop"
             $WarningPreference = "SilentlyContinue"
             $output = @{}
             $data = ${command}
             $output['${Constants.AzVersion}'] = $data.Version.ToString()
             $output['${Constants.Success}'] = "true"
+            $secs = $curr.Elapsed.TotalSeconds
+            echo "time elapsed: $secs"
         }
         catch {
             $output['${Constants.Error}'] = $_.exception.Message
         }
-        $secs = $curr.Elapsed.TotalSeconds
-        echo "time elapsed: $secs"
         return ConvertTo-Json $output`;
         core.debug(`GetLatestModuleScript: ${this.script}`);
         return this.script;
