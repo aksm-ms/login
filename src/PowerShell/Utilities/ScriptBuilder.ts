119a93c5234cea9b6f9841f85af6f2f15750779a
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-05-19 12:52:10 +0530

    added time taken for script

--- src/PowerShell/Utilities/ScriptBuilder.ts
+++ src/PowerShell/Utilities/ScriptBuilder.ts
@@ -11,17 +11,21 @@ export default class ScriptBuilder {
         if (scheme === Constants.ServicePrincipal) {
             command += `Connect-AzAccount -ServicePrincipal -Tenant ${tenantId} -Credential \
             (New-Object System.Management.Automation.PSCredential('${args.servicePrincipalId}',(ConvertTo-SecureString ${args.servicePrincipalKey} -AsPlainText -Force))) \
-                -Environment ${args.environment} | out-null;`;
+                -Environment ${args.environment} -Debug 5>&1`;
+            // command += `Connect-AzAccount -ServicePrincipal -Tenant ${tenantId} -Credential \
+            // (New-Object System.Management.Automation.PSCredential('${args.servicePrincipalId}',(ConvertTo-SecureString ${args.servicePrincipalKey} -AsPlainText -Force))) \
+            //     -Environment ${args.environment} | out-null;`;
             if (args.scopeLevel === Constants.Subscription) {
                 command += `Set-AzContext -SubscriptionId ${args.subscriptionId} -TenantId ${tenantId} | out-null;`;
             }
         }
         this.script += `try {
+            $DebugPreference = "Continue"
             $curr = [system.diagnostics.stopwatch]::startNew()
             $ErrorActionPreference = "Stop"
             $WarningPreference = "SilentlyContinue"
             $output = @{}
-            ${command}
+            ${command} 
             $output['${Constants.Success}'] = "true"
             $output['secs'] = $curr.Elapsed.TotalSeconds.ToString()
         }
